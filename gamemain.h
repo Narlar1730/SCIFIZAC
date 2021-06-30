23d6702ca68836e870f46a0f1b91306185803aa6
Erik Tults <e.tults@bigpond.com>
2021-06-30 11:40:07 +1000

    Setup main game loop, wrote logic for moving main character around, wrote logic for controlling gun direction - this is not very good and will need to be fixed. Seperated game logic into seperate thread to drawing the screen. This just seemed logical, but like all things, will likely bite me in the ass at some point later on.

--- gamemain.h
+++ gamemain.h
@@ -12,6 +12,11 @@
 
 
 using namespace std;
+//Handle interface running
+bool runningScreen = true;
+
+
+//Handle Characters
 vector<char> charPressed ={};
 
 bool Dpressed = false;
@@ -30,10 +35,11 @@ bool RIGHTpressed = false;
 class Player {
 	int xpos, ypos;
 	float xVel, yVel;
-
+	int speed = 5;
 	public:
 		void setInitValues(int, int);
 		void drawPlayer(sf::RenderWindow*);
+		void MoveCharacter();
 };
 
 void Player::setInitValues(int x, int y)
@@ -46,16 +52,73 @@ void Player::setInitValues(int x, int y)
 
 void Player::drawPlayer(sf::RenderWindow* window)
 {
-	sf::Color PlayerColor {160, 160, 180};
+	//Draw Player
+	sf::Color PlayerColor {160, 160, 200};
 	sf::Color black {0, 0, 0};
+	sf::Color GunGrey {120, 120, 120};
 
 	sf::CircleShape circle;
-	circle.setRadius(150);
+	circle.setRadius(40);
 	circle.setFillColor(PlayerColor);
 	circle.setOutlineColor(black);
 	circle.setOutlineThickness(4);
 	circle.setPosition(xpos, ypos);
 
+
+	//Draw gun
+	//Gun doesn't point the way I like. Will come back to this later
+	//FIXME
+	bool DrawGun = false;
+	vector<int> angles = {};
+	int arrowsPressed = 0;
+	if(RIGHTpressed)
+	{
+		angles.push_back(0);	
+		DrawGun = true;
+		arrowsPressed+=1;
+	}
+	if(LEFTpressed)
+	{
+		angles.push_back(180);
+		DrawGun = true;
+		arrowsPressed+=1;
+	}
+	if(UPpressed)
+	{
+		angles.push_back(270);
+		DrawGun = true;
+		arrowsPressed+=1;
+	}
+	if(DOWNpressed)
+	{
+		angles.push_back(90);
+		DrawGun = true;
+		arrowsPressed+=1;
+	}
+	if(DrawGun and arrowsPressed < 3)
+	{
+		int angle = 0;
+		int len = angles.size();
+		for(int i = 0; i < len; i++)
+		{
+			angle += angles[i];
+		}	
+		angle = angle / len;
+		sf::RectangleShape gun(sf::Vector2f(55.f, 10.f));
+		gun.setPosition(xpos+35, ypos+35);
+		gun.setFillColor(GunGrey);
+		gun.setOutlineColor(black);
+		gun.setOutlineThickness(4);
+		if(RIGHTpressed && UPpressed)
+		{
+			angle = 315;
+		}
+		gun.setRotation(angle);
+		window->draw(gun);
+	}
+
+
+	//Draw objects in the right order
 	window->draw(circle);
 
 }
@@ -92,14 +155,88 @@ void CharsPressed()
 }
 char DrawGameScreen(int, int, bool, Player, sf::RenderWindow*);
 
+void Player::MoveCharacter()
+{
+	//Change Speed based on arrow key pressed
+	if(Apressed)
+	{
+		xVel = xVel-speed;
+	}
+	if(Dpressed)
+	{
+		xVel = xVel+speed;
+	}
+	if(Wpressed)
+	{
+		yVel = yVel-speed;
+	}
+	if(Spressed)
+	{
+		yVel = yVel+speed;
+	}
+
+	//Slow down chracter
+	if(xVel > 0)
+	{
+		xVel -= 1;
+	}
+	else if(xVel < 0)
+	{
+		xVel +=1;
+	}
+
+	if(yVel > 0)
+	{
+		yVel -= 1;
+	}
+	else if(yVel < 0)
+	{
+		yVel += 1;
+	}
+	//set max speed, currently set to be a static 5, but could be changed.
+	int topspeed = 5;
+
+	if(xVel > topspeed)
+	{
+		xVel = topspeed;
+	}
+	else if(xVel < -1*topspeed)
+	{
+		xVel = -1*topspeed;
+	}
+	if(yVel > topspeed)
+	{
+		yVel = topspeed;
+	}
+	else if(yVel < -1*topspeed)
+	{
+		yVel = -1*topspeed;
+	}
+	xpos = xpos + xVel;
+	ypos = ypos + yVel;
+
+}
+
+Player mainChar;
+
+void playGameThread()
+{
+	while (runningScreen)
+	{
+		mainChar.MoveCharacter();
+		unsigned int microsecond = 1000000;
+		usleep(0.005*microsecond);
+	}
+}
 
 void GameScreen(sf::RenderWindow* window)
 {
 	bool screenRunning = true;
 	char next;
-	Player mainChar;
 	mainChar.setInitValues(900, 900);
-	while(screenRunning)
+	sf::Thread thread(&playGameThread);
+	thread.launch();
+	while(runningScreen)
 	{
 
 		sf::Event event;
@@ -128,6 +265,9 @@ void GameScreen(sf::RenderWindow* window)
 		{
 			cout << charPressed[i];
 		}
+		/*
+		mainChar.MoveCharacter();
+		*/
 		cout << "\n";
 
 		if(next != 's')
@@ -135,19 +275,25 @@ void GameScreen(sf::RenderWindow* window)
 			screenRunning = false;
 		}
 
+		
+		/*
 		unsigned int microsecond = 1000000;
-		usleep(0.015*microsecond);
-
+		usleep(0.005*microsecond);
+		*/
 	}
 
 }
 
 char DrawGameScreen(int mousex, int mousey, bool MouseReleased, Player mainChar, sf::RenderWindow* window)
 {
+	//Init variables, colours, etc.
 	char outPut = 's';
 	sf::Color backgroundColour {160, 160, 160};
 	sf::Color black {0, 0, 0};
 	
+	//Clear the window before we draw
+	window->clear();
+
 	//Draw background
 	sf::RectangleShape background(sf::Vector2f(1800.f, 1800.f));
 	background.setPosition(0, 0);
